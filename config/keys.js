304434c92d1c8e7bd76d35c95b9a9c15e6721d48
Zohaib Afridi <zohaibafridi@zohaibs-air.lan>
2019-04-17 15:52:40 -0400

    began with adding registration endpoint

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // %40 in the password section = the @ sign
+  // meaning, my password has the @ sign in it
+  mongoURI: 'mongodb://zohaib0126:Finalfantasy%409@ds143326.mlab.com:43326/to-do',
+  secretOrKey: 'secret'
+}
