c2a5ad17ba7ed8171f040c189502788e79618517
Zohaib Afridi <zohaibafridi@zohaibs-air.lan>
2019-04-19 16:24:12 -0400

    moved code from master

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ require('./config/passport')(passport)
 app.use('/api/users', users)
 
 const port = process.env.PORT || 5000
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`))
