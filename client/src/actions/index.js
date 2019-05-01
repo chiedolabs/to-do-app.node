469794dff62d883e9c5944e5e4886dc3ea403973
Zohaib Afridi <zohaibafridi@zohaibs-air.lan>
2019-05-01 15:00:21 -0400

    action creators for todo list

---
+++ client/src/actions/index.js
@@ -0,0 +1,23 @@
+let nextToDoId = 0
+
+export const addTodo = text => ({
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+})
+
+export const setVisibilityFilter = filter => ({
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+})
+
+export const toggleTodo = id => ({
+    type: 'TOGGLE_TODO',
+    filter
+})
+
+export const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
