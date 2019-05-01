2fb8d415b1854bc81659abba8ce34d79b885d3ea
Zohaib Afridi <zohaibafridi@zohaibs-air.lan>
2019-05-01 13:16:13 -0400

    linked redux to login

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { registerUser } from '../../actions/authActions'
@@ -134,7 +134,7 @@ class Register extends Component {
     }
 }
 
-Register.PropTypes = {
+Register.propTypes = {
     registerUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired
