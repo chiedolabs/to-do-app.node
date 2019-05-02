675c849c677d8f4a7920bc290d20c3ea2269ebd9
Zohaib Afridi <zohaibafridi@zohaibs-air.lan>
2019-05-02 14:13:36 -0400

    added logout button to todo list screen

--- client/src/components/AppFront.js
+++ client/src/components/AppFront.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
@@ -7,20 +7,53 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { logoutUser } from '../actions/authActions'
 
-const AppFront = () => (
-    <div>
-        <AddTodo />
-        <VisibleTodoList />
-        <Footer />
-    </div>
-)
+// const AppFront = () => (
+//     <div>
+//         <AddTodo />
+//         <VisibleTodoList />
+//         <Footer />
+//     </div>
+// )
 
-// class AppFront extends Component {
-//     onLogoutClick = e => {
-//         e.preventDefault()
-//         this.props.logoutUser()
-//     }
-// }
+class AppFront extends Component {
+    onLogoutClick = e => {
+        e.preventDefault()
+        this.props.logoutUser()
+    }
+    render() {
+        
+        const { user } = this.props.auth
+        return (
+            <div>
+                <AddTodo />
+                <VisibleTodoList />
+                <Footer />
+            <div style={{ height: '75vh' }} className='container valign-wrapper'>
+                <div className='row'>
+                    <div className='col s12 center-align'>
+                        <h4>
+                            <b>Hey there,</b> { user.name.split(' ')[0] }
+                            <p className='flow-text grey-text text-darken-1'>
+                            You are logged in
+                            </p>
+                        </h4>
+                        <button style={{
+                            width: '150px',
+                            borderRadius: '3px',
+                            letterSpacing: '1.5px',
+                            marginTop: '1rem'
+                        }}
+                        onClick={ this.onLogoutClick }
+                        className='btn btn-large waves-effect waves-light hoverable blue accent-3'
+                        >Logout
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
 
 AppFront.propTypes = {
     logoutUser: PropTypes.func.isRequired,
